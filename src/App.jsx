b0cacbf4eada84d0ccec3f09fd2e7df9090b8e28
Your_Name <musharafgujjar7711@gmail.com>
2024-11-07 20:56:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from './components/Head'
+import Button from './components/Button'
+import ButtonGradient from './assets/assets/svg/ButtonGradient'
+function App() {
+  return (
+    <div>
+    <div className='pt-[4.75rem] lg:pt-[5.24rem] overflow-hidden'>
+    <Head/>
+    </div>
+      <ButtonGradient/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Head.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { hero } from '../assets/assets'
+import { brainwave } from '../assets/assets'
+import { navigation } from '../constant'
+import { useLocation } from 'react-router-dom'
+import Menusvg from '../assets/assets/svg/MenuSvg'
+import {HamburgerMenu} from './design/Header'
+import Button from './Button'
+function Head() {
+    const [openNavigation ,setOpenNavigation] = useState(true)
+    const pathname= useLocation()
+  return (
+    <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:backdrop-blur-sm
+    lg:bg-n-8/90 ${openNavigation ? 'bg-n-8 ':'bg-n-8/80 backdrop-blur-sm '}`}>   
+        <div className=' flex items-center px-5 lg:px-7.5 xl:px-10  max-lg:py-4'>
+             <a className='w-[12rem] block' href="#hero">
+             <img src={brainwave} alt="brainwave" width={190} height={40} />
+             </a>
+             <nav className= {`${openNavigation ? 'flex flex-col' :'hidden'}  fixed  top-[5rem] left-0 right-0 lg:static lg:flex lg:mx-auto lg:bg-transparent bg-n-8`}>
+                     <div className=' flex flex-col items-center justify-center m-auto lg:flex-row'>
+                       {navigation.map((item)=>(
+                        <a key={item.id} href={item.url} className={`block  relative font-code uppercase text-2xl text-n-1 hover:text-color-1 ${item.onlyMobile ? 'lg:hidden' :''}
+                        px-6 py-6 md:py-8 lg:text-xs lg:font-semibold  ${item.url ===pathname.hash ?'z-2 lg:text-n-1' : 'text-n-1/50'} lg:leading-5 xl:px-12 hover:text-n-1`}>
+                            {item.title}
+                            
+                        </a>
+                       ))}
+                       <HamburgerMenu/>
+                     </div>
+             </nav>
+             
+              <div className={`flex items-center gap-4 `}>
+              <a href="" className={`button transition-colors  text-n-4 hover:text-n-1 lg:block 
+                hidden`}>
+                     singup
+                </a>
+            <Button className='hidden lg:flex ' href='#login'>
+                Sign in
+            </Button>
+              </div>
+        </div>
+     </div>
+  )
+}
+
+export default Head
